0125da2c50954aaab003ac88104a19f3c2f2a9ce
isinhah <6isinha6@gmail.com>
2023-09-03 13:45:20 -0300

    add criação do objeto usuario

--- estante/src/Estante.java
+++ estante/src/Estante.java
@@ -1,5 +1,33 @@
+
+import java.util.Scanner;
+
 public class Estante {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        // Inserir dados do usuário
+        System.out.println("INSERIR DADOS");
+        System.out.println("--------------------");
+        System.out.print("Insira seu nome: ");
+        String nome = sc.next();
+        sc.nextLine(); // consome a linha pendente
+        System.out.print("Insira seu username: ");
+        String username = sc.next();
+        sc.nextLine(); // consome a linha pendente
+        System.out.print("Nota do livro (1 a 10): ");
+        double nota = sc.nextDouble();
+        sc.nextLine(); // consome a linha pendente
+        System.out.print("Comentário sobre o livro: ");
+        String comentario = sc.nextLine();
+
+        // Instanciação do objeto Usuario
+        Usuario usuario = new Usuario(nome, username, comentario, nota);
+
+        // Print do usuário
+        System.out.println(usuario.imprimirUsuario());
+
+        sc.close();
+
     }
 }
--- estante/src/Usuario.java
+++ estante/src/Usuario.java
@@ -1,3 +1,59 @@
 public class Usuario {
+  private String nome;
+  private String username;
+  private String comentario;
+  private double nota;
 
+  // construtor --> inicializa o atributo com o valor do parâmetro
+  public Usuario(String nome, String username, String comentario, double nota) {
+    this.nome = nome;
+    this.username = username;
+    this.comentario = comentario;
+    this.nota = nota;
+  }
+
+  // metodos
+
+  // método que poupa o código de imprimir o perfil do usuário, na classe Estante
+  public String imprimirUsuario() {
+    return "\n" + "USUARIO" + "\n" + "--------------------" + "\n" + "Nome: " + getNome() + "\n" + "Username: "
+        + getUsername() + "\n"
+        + "Nota do livro: "
+        + getNota() + "\n" + "Comentário: " + getComentario();
+  }
+
+  // método que trata a nota (entre 1 e 10)
+
+  // getters e setters --> retorna e define o valor do atributo
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getComentario() {
+    return comentario;
+  }
+
+  public void setComentario(String comentario) {
+    this.comentario = comentario;
+  }
+
+  public double getNota() {
+    return nota;
+  }
+
+  public void setNota(double nota) {
+    this.nota = nota;
+  }
 }
